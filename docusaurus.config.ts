e650cd52e96c21015663df9f58643d65744fa00d
subhadip001 <ssddmm2001@gmail.com>
2025-06-27 14:18:19 +0530

    Add color mode configuration to Docusaurus settings, enabling dark mode by default and respecting user preferences for improved accessibility and user experience.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -86,6 +86,11 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      defaultMode: 'dark',
+      respectPrefersColorScheme: true,
+      disableSwitch: false,
+    },
     navbar: {
       title: "Layerpath Docs",
       logo: {
